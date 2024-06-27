781b429077e5c876fa85cf789d43f72b1cf912a7
jeanga7 <jeangabrielgoudiaby@gmail.com>
2024-06-27 14:39:43 +0000

    push

---
+++ app/jeu/page.js
@@ -0,0 +1,24 @@
+"use client"
+
+import { useState } from "react"
+
+const jeu = () => {
+
+    const [number, setNumber] = useState(0)
+    const increment = () => {
+        setNumber(number + 1)
+    }
+    const decrement = () => {
+        setNumber(number - 1)
+    }
+
+    return (
+        <>
+            <div>{number}</div>
+            <button onClick={() => increment()}>Increment</button>
+            <button onClick={() => decrement()}>Decrement</button>
+        </>
+    )
+}
+
+export default jeu
